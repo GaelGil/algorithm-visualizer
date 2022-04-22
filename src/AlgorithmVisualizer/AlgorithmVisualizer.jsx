0501b88f34a282edda92e9da641c2c1e0f86ca62
GaelGil <gaelgilbello27@gmail.com>
2022-04-22 15:48:35 -0700

    working on displaying numbers

--- src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
+++ src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
@@ -6,20 +6,39 @@ import './AlgorithmVisualizer.css';
 export default class AlgorithmVisualizer extends React.Component{
     constructor(props) {
         super(props);
-        this.state = { array : [1, 2, 3, 4, 5] };
+        this.state = { array : [] };
       }
 
+
+    setArray(){
+        let array = [];
+        for (let i = 0; i < 100; i ++){
+            array.push(randInts(5, 1000));
+        }
+        this.setState({array})
+    }
+
+    componentDidMount() {
+        this.setArray();
+    }
 
     render() {
         const {array} = this.state;
 
         return (
-            <>
+            <div className="array">
             {array.map((value, index) => (
                 <div className="array-bar" key={index}>{value}</div>
             ))}
-            </>
+            </div>
         )
     };
 
+}
+
+
+function randInts(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
