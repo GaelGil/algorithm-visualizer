67e807d97a6fc6ba0355d52fdbb92c7947d978d6
Gael <gaelgilbello27@gmail.com>
2023-10-17 10:21:49 -0700

    coloring the grid

--- src/components/GraphTraversalVisualiser/GraphTraversalVisualiser.jsx
+++ src/components/GraphTraversalVisualiser/GraphTraversalVisualiser.jsx
@@ -26,51 +26,24 @@ const GraphTraversalVisualiser = () => {
       newGraph.push(row);
     } 
       setGraphVisuals(newGraph);
+      changeAllGridItemColors();
       setGraph(newGraph);
   };
-
   const setGraphVisuals = (graph) => {
     const graphItem = document.getElementsByClassName('grid-item');
-    // const graph =
-
-    console.log(Object.keys(graphItem));
-    for (let i = 0; i <= graphItem.length; i ++){
-      // for (let j = 0; j <= graphItem[i].length; j++){
-        // barOneStyle.backgroundColor = color;
-        // const barOneStyle = arrayBars[barOneIdx].style; 
-      console.log("4");
-        // console.log(graphItem[i][j])
-        // graphItem[i][j].style.backgroundColor = "red";
-      // }
+    for (var i = 0; i < graphItem.length; i++) {
+      graphItem[i].style.backgroundColor ="red";
+      if (graph[i] === 's'){
+        graphItem[i].style.backgroundColor ="red";
+      }
+      else if (graph[i] === 'w'){
+        graphItem[i].style.backgroundColor ="black";
+      }
+      if (graph[i] === 'o'){
+        graphItem[i].style.backgroundColor ="yellow";
+      }    
     }
-
-
-
-
-    // const arrayBars = document.getElementsByClassName('array-bar'); // select the array bars html
-    // let current_animation = {'compare': []}; // we compare this to the first animation
-    //   for (let i = 0; i < animations.length; i++) {
-    //     // if the current item in animations is a dictionary change the color
-    //     if (animations[i].constructor === Object){
-    //       const barOneIdx = (animations[i].compare[0]); // select the first bar index
-    //       const barTwoIdx = (animations[i].compare[1]); // select the second bar index
-    //       const barOneStyle = arrayBars[barOneIdx].style; 
-    //       const barTwoStyle = arrayBars[barTwoIdx].style;
-    //       let color = PRIMARY_COLOR; 
-    //       // If the animation is the same as the previous one this means that we are only adding it
-    //       // to show the values we are currently comparing. So we change the color of them.
-    //       if (JSON.stringify(animations[i].compare) === JSON.stringify(current_animation.compare)){
-    //         color = SECONDARY_COLOR;
-    //       }
-    //       current_animation = animations[i];
-    //       setTimeout(() => {
-    //         barOneStyle.backgroundColor = color;
-    //         barTwoStyle.backgroundColor = color;
-    //       }, i * ANIMATION_SPEED_MS);
-    //     } 
   }
-
-
   
 
   const handleSubmit = (event) => {
@@ -78,17 +51,17 @@ const GraphTraversalVisualiser = () => {
     let method = algorithm;
     let time = 0;
     disableButtons();
-    if (method === 'Merge') {
+    if (method === 'astar') {
       // time = startSorting(getMergeSortAnimations(array));
       // time = mergeSort();
-    } else if (method === 'Bubble') {
+    } else if (method === 'BFS') {
       // time = startSorting(getBubbleSortAnimations(array));
     } else if (method === 'Reset') {
       resetGraph();
       enableButtons(1);
-    } else if (method === 'Insertion') {
+    } else if (method === 'DFS') {
       // time = startSorting(getInsertionSortAnimations(array));
-    } else if (method === 'Selection') {
+    } else if (method === 'UCS') {
       // time = startSorting(getSelectionSortAnimations(array));
 
     }
@@ -119,7 +92,7 @@ const GraphTraversalVisualiser = () => {
         <table>
           <tbody>
             {graph.map((row, rowIndex) => (
-              <tr key={rowIndex}>
+              <tr key={rowIndex} className={"row-index"}>
                 {row.map((cell, cellIndex) => (
                   <td
                     key={cellIndex}
