c5ad165fd3914b519db2932a8d8100ed05c891bc
Gael <gaelgilbello27@gmail.com>
2024-01-10 19:18:23 -0800

    implementing algorithms

--- src/components/GraphTraversalVisualiser/graphAlgorithms/bfs.js
+++ src/components/GraphTraversalVisualiser/graphAlgorithms/bfs.js
@@ -1,28 +1,84 @@
-import { Stack } from './datastructures/stack';
+function isValidIndex(matrix, i, j) {
+    return i >= 0 && i < matrix.length && j >= 0 && j < matrix[0].length;
+}
 
 
-export function BFS(grid, start, destination){
-    // let root = start;
-    let queue = [start];
-    let path = {}
-    let expanded = {}
-    let visited = {}
-    while (queue){
-        let current_node = queue[queue.length-1]
-        // stack.remove(stack.length-1)
-        if (current_node === destination){
-            return path
-        }
+function getNeighbors(matrix, current_node) {
+    let neighbors = []
+    let i = current_node[0];
+    let j = current_node[1];
+    // top neihgbor
+    if (isValidIndex(matrix, i - 1, j)) {
+        neighbors.push([i - 1, j]);
+    }
+
+    // bottom neighbor
+    if (isValidIndex(matrix, i + 1, j)) {
+        neighbors.push([i + 1, j]);
+    }
+
+    // left neighbor
+    if (isValidIndex(matrix, i, j - 1)) {
+        neighbors.push([i, j-1]);
 
     }
-        // let current_node = stack.pop()
-        // if current_node == destination;
-        // solution found 
-        // else if current_node not in visited and current_node in grid:
-            // visited[curernt_node] = 0
-            // get neighbors 
-            // append_neihbors to stack
-    // get neighbors
-    return 0
+
+    // right neighbor
+    if (isValidIndex(matrix, i, j + 1)) {
+        neighbors.push([i, j+1]);
+
+    }
+
+
+    return neighbors;
 }
+
+
+
+
+
+// Example Usage
+const matrix = [
+    ["s", "0", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"],
+    ["0", "0", "0", "0", "o"],
+];
+
+
+
+
+
+
+export function BFS(grid, start, destination) {
+    let queue = [start];
+    let visited = {};
+
+    while (queue.length > 0) {
+        // console.log(queue);
+        // fix the checking if visited
+        let current_node = queue.shift();  // Use shift to remove the first element (FIFO for BFS)
+        console.log(current_node);
+        if (current_node === destination) {
+            return "found";
+        } else { 
+            let current_node_key = JSON.stringify(current_node);
+            // console.log(current_node_key)
+            if (!(visited.hasOwnProperty(current_node_key))) {
+                // console.log("here");
+                visited[current_node_key] = current_node;
+                let neighbors = getNeighbors(grid, current_node);
+                // console.log(neighbors)
+                for (let i = 0; i < neighbors.length; i++) {
+                    queue.push(neighbors[i]);
+                }
+            }
+        }
+    }
+
+    return "not found";
+}
+
 
--- src/components/GraphTraversalVisualiser/graphAlgorithms/dfs.js
+++ src/components/GraphTraversalVisualiser/graphAlgorithms/dfs.js
@@ -1,20 +1,35 @@
-import { Stack } from './datastructures/stack';
 
 
 export function DFS(grid, start, destination){
-    // let root = start;
-    let queue = [start];
-    let path = {}
-    let expanded = {}
-    let visited = {}
-    while (queue){
-        let current_node = queue[queue.length-1]
-        // stack.remove(stack.length-1)
-        if (current_node === destination){
-            return path
-        }
+    // let queue = [start];
+    // let visited = {}
+    // // let path = []
 
-    }
+    // while (queue) {
+    //     let current_node = queue.splice(-1);
+    //     if (current_node === destination){
+    //         return "found";
+    //     }
+    //     else {
+
+    //     }
+    // }
+
+
+
+    // let root = start;
+    // let queue = [start];
+    // let path = {}
+    // let expanded = {}
+    // let visited = {}
+    // while (queue){
+    //     let current_node = queue[queue.length-1]
+    //     // stack.remove(stack.length-1)
+    //     if (current_node === destination){
+    //         return path
+    //     }
+
+    // }
         // let current_node = stack.pop()
         // if current_node == destination;
         // solution found 
