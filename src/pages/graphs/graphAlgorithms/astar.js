53c8aa1a4e7c4caabcbb65bf8ff515bb8b8b7f9f
gaelgil <gaelgilbello27@gmail.com>
2025-05-17 15:38:28 -0700

    fixed astar implementation

--- src/pages/graphs/graphAlgorithms/astar.js
+++ src/pages/graphs/graphAlgorithms/astar.js
@@ -1,37 +1,38 @@
 import { PriorityQueue } from './datastructures/priorityQueue';
-import { getNeighbors, heuristic } from './helper';
+import { getNeighbors, heuristic, exists, randInts } from './helper';
 
 export function ASTAR(grid, start, destination) {
-    let expanded = []; // expanded nodes (nodes that weve checked all neighbors)
     let priorityQueue = new PriorityQueue(); // prioirty queue with lowest cost priority
-    priorityQueue.enqueue({ node: start, path: [start], cost: heuristic(start, destination)});
+    priorityQueue.enqueue({ node: start, path: [start], cost: heuristic(start, destination)}); // add start node with cost being heuristic value
+    let expanded = []; // expanded nodes (nodes that weve checked all neighbors)
+
 
     while (!priorityQueue.isEmpty()) {
         let { node, path, cost } = priorityQueue.dequeue(); // get the node, its path, and cost from the front of the priority queue
-        let nodeKey = node.toString(); // to string for comparison
 
-        if (expanded.includes(node)){
-            continue;
-        }
-        expanded.push(node); 
-
-        if (nodeKey === destination.toString()) { // check if we have arrived at the solution
-            return { status: "found", path, cost , expanded};
+        if (node.toString() === destination.toString()) { // if node is destination 
+            return { status: "found", path, cost, expanded }; // return found, path, cost, and expanded. 
         }
 
-        let neighbors = getNeighbors(grid, node);
-        for (let i = 0; i < neighbors.length; i++){
-            let neighbor = neighbors[i];
-            let costToNeighbor = grid[neighbor[0]][neighbor[1]] === "e" ? 5 : 1; 
-            let g = cost + costToNeighbor;
-            let f = g + heuristic(neighbor, destination); // f = g (total cost to node) + heuristic
+        if (exists(expanded, node)){ // if we have already expanded this node continue
+            continue; 
+        }
 
-            if (grid[neighbor[0]][neighbor[1]] !== "w") { // not a wall
-                if (f < costToNeighbor) {
-                    priorityQueue.enqueue({ node: neighbor, path: path.concat([neighbor]), cost: f });
-                }
+        let neighbors = getNeighbors(grid, node); // get neighbors of current node
+        for (let i = 0; i < neighbors.length; i++){ // for all neighbors
+            let neighbor = neighbors[i]; // select neighbor
+            if (grid[neighbor[0]][neighbor[1]] !== "w") { // if not a wall
+                let costToNeighbor = 1; // set cost to neighbor as 1
+                if (grid[neighbor[0]][neighbor[1]] === 'e'){ // if weighted 
+                    costToNeighbor = randInts(5, 10); // set random weight cost
+                    }
+                let g = cost + costToNeighbor; // add up total cost actual to neighbor node 
+                let f = g + heuristic(neighbor, destination); // f = g (total cost to node) + heuristic
+                priorityQueue.enqueue({ node: neighbor, path: path.concat([neighbor]), cost: f }); // add neihhbor, path and f cost
             }
         }
+        expanded.push(node); // add to expanded list
+
     }
 
     return { status: "not found", path: [], cost: 0 };
