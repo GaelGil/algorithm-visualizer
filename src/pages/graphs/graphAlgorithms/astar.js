2836be4d166dce51f8f3e841239bead9c3a99f90
gaelgil <gaelgilbello27@gmail.com>
2025-05-16 22:59:47 -0700

    updade bfs and dfs to clearer code

--- src/pages/graphs/graphAlgorithms/astar.js
+++ src/pages/graphs/graphAlgorithms/astar.js
@@ -2,39 +2,36 @@ import { PriorityQueue } from './datastructures/priorityQueue';
 import { getNeighbors, heuristic } from './helper';
 
 export function ASTAR(grid, start, destination) {
-    let visited = {};
-    let expanded = [];
-    let priorityQueue = new PriorityQueue();
+    let expanded = []; // expanded nodes (nodes that weve checked all neighbors)
+    let priorityQueue = new PriorityQueue(); // prioirty queue with lowest cost priority
     priorityQueue.enqueue({ node: start, path: [start], cost: heuristic(start, destination)});
 
     while (!priorityQueue.isEmpty()) {
         let { node, path, cost } = priorityQueue.dequeue(); // get the node, its path, and cost from the front of the priority queue
         let nodeKey = node.toString(); // to string for comparison
 
-        if (visited[nodeKey]){
+        if (expanded.includes(node)){
             continue;
         }
+        expanded.push(node); 
 
         if (nodeKey === destination.toString()) { // check if we have arrived at the solution
             return { status: "found", path, cost , expanded};
         }
 
-        visited[nodeKey] = true;
         let neighbors = getNeighbors(grid, node);
         for (let i = 0; i < neighbors.length; i++){
             let neighbor = neighbors[i];
-            let costToNeighbor = cost;
-            costToNeighbor = grid[neighbor[0]][neighbor[1]] === "e" ? 5 : 1; 
-
-            let f = heuristic(neighbor, destination) + costToNeighbor;
+            let costToNeighbor = grid[neighbor[0]][neighbor[1]] === "e" ? 5 : 1; 
+            let g = cost + costToNeighbor;
+            let f = g + heuristic(neighbor, destination); // f = g (total cost to node) + heuristic
 
             if (grid[neighbor[0]][neighbor[1]] !== "w") { // not a wall
-                priorityQueue.enqueue({ node: neighbor, path: path.concat([neighbor]), cost: f });
-                expanded.push(neighbor); 
+                if (f < costToNeighbor) {
+                    priorityQueue.enqueue({ node: neighbor, path: path.concat([neighbor]), cost: f });
+                }
             }
         }
-
-
     }
 
     return { status: "not found", path: [], cost: 0 };
