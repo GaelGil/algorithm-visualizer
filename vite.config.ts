dc2a1c1468fc6935463853a13e352d34b76816bf
Gael <gaelgilbello27@gmail.com>
2025-08-20 21:11:07 -0700

    updating to bun

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 base: "/YOUR_REPOSITORY_NAME";
 import react from "@vitejs/plugin-react";
-
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "/algorithm-visualizer",
 });
