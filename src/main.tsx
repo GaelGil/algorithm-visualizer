fb198a7770651c335e091966ff4886f1655d3625
Gael <gaelgilbello27@gmail.com>
2025-09-23 23:45:21 -0700

    updating for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/algorithm-visualizer">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
